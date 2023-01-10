7b1c823b66b4788b3998902bf4962bfb49530593
Sonik <sonik@noreply.example.org>
2023-01-10 15:11:20 +0000

    Update 'database/db.sql'

---
+++ database/db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE User (
+	ID numeric PRIMARY KEY AUTOINCREMENT,
+	username varchar,
+	password text,
+	email text
+);
+
+CREATE TABLE Post (
+	ID numeric PRIMARY KEY AUTOINCREMENT,
+	title varchar,
+	text text,
+	data datetime,
+	UserID numeric
+);
+
+CREATE TABLE Category (
+	ID numeric PRIMARY KEY AUTOINCREMENT,
+	title varchar
+);
+
+CREATE TABLE Commet (
+	ID numeric PRIMARY KEY AUTOINCREMENT,
+	text text,
+	data datetime,
+	UserID numeric,
+	PostID numeric
+);
