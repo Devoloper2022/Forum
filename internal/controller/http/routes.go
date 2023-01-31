91e0214cac8ac621dc9683254e2c5cb06316eef7
Your Name <you@example.com>
2023-01-31 14:22:37 +0600

    Filter

--- internal/controller/http/routes.go
+++ internal/controller/http/routes.go
@@ -16,26 +16,29 @@ const (
 
 	// post
 	urlPostCreate = "/post/create"
-	urlPostDelete = "/post/delete"
-	urlPostUpdate = "/post/update"
-	urlPost       = "/post/"
-	urlPosts      = "/post/all"
-	urlPostLike   = "/post/like"
-	urlFilter     = "/filter/all"
+	// urlPostDelete = "/post/delete"
+	// urlPostUpdate = "/post/update"
+	urlPost = "/post/"
+	// urlPosts    = "/post/all"
+	urlPostLike = "/post/like"
+
+	urlFilterCategory = "/filter/category/"
+	urlFilterLike     = "/filter/like"
+	urlFilterDislike  = "/filter/dislike/"
 
 	// user
-	urlUser        = "/user/"
-	urlUserProfile = "/profile"
-	urlUserUpdate  = "/user/update"
-	urlUserDelete  = "/user/delete"
+	urlUser = "/user/"
+	// urlUserProfile = "/profile"
+	// urlUserUpdate = "/user/update"
+	// urlUserDelete = "/user/delete"
 
 	// comment
 	urlCommentCreate = "/comment/create"
-	urlCommentDelete = "/comment/delete"
-	urlCommentUpdate = "/comment/update "
-	urlComment       = "/comment/"
-	urlComments      = "/comments/all/"
-	urlCommentLike   = "/comment/like"
+	// urlCommentDelete = "/comment/delete"
+	// urlCommentUpdate = "/comment/update "
+	// urlComment     = "/comment/"
+	urlComments    = "/comments/all/"
+	urlCommentLike = "/comment/like"
 )
 
 func (h *Handler) Routes() *http.ServeMux {
@@ -57,26 +60,30 @@ func (h *Handler) Routes() *http.ServeMux {
 	mux.HandleFunc(urlLogout, m.chain(h.Logout))   // for auth
 
 	// User
-	mux.HandleFunc(urlUser, m.chain(h.GetUser))        // for auth
-	mux.HandleFunc(urlUserProfile, m.chain(h.profile)) // for owner
+	mux.HandleFunc(urlUser, m.chain(h.GetUser)) // for auth
+	// mux.HandleFunc(urlUserProfile, m.chain(h.profile)) // for owner
 	// mux.HandleFunc(urlUserDelete, h.)       	// for owner
 	// mux.HandleFunc(urlUserUpdate, h.)         // for owner
 
 	// post
-	mux.HandleFunc(urlPost, m.chain(h.GetPost))          // for all
-	mux.HandleFunc(urlPosts, m.chain(h.ListPosts))       // for all
+	mux.HandleFunc(urlPost, m.chain(h.GetPost)) // for all
+	// mux.HandleFunc(urlPosts, m.chain(h.ListPosts))       // for all
 	mux.HandleFunc(urlPostCreate, m.chain(h.CreatePost)) // for auth
-	mux.HandleFunc(urlPostDelete, m.chain(h.DeletePost)) // for owner
-	mux.HandleFunc(urlPostUpdate, m.chain(h.UpdatePost)) // for owner
-	mux.HandleFunc(urlPostLike, m.chain(h.LikePost))     // for owner
+	// mux.HandleFunc(urlPostDelete, m.chain(h.DeletePost)) // for owner
+	// mux.HandleFunc(urlPostUpdate, m.chain(h.UpdatePost)) // for owner
+	mux.HandleFunc(urlPostLike, m.chain(h.LikePost)) // for owner
+
+	mux.HandleFunc(urlFilterCategory, m.chain(h.ListPosts))      // for all
+	mux.HandleFunc(urlFilterDislike, m.chain(h.ListPostsByLike)) // for all
+	mux.HandleFunc(urlFilterLike, m.chain(h.ListPostsByLike))    // for all
 
 	// comment
-	mux.HandleFunc(urlComment, m.chain(h.GetPost))             // for all
+	// mux.HandleFunc(urlComment, m.chain(h.GetPost))             // for all
 	mux.HandleFunc(urlComments, m.chain(h.ListComments))       // for all
 	mux.HandleFunc(urlCommentCreate, m.chain(h.CreateComment)) // for auth
-	mux.HandleFunc(urlCommentDelete, m.chain(h.DeletePost))    // for owner
-	mux.HandleFunc(urlCommentUpdate, m.chain(h.UpdatePost))    // for owner
-	mux.HandleFunc(urlCommentLike, m.chain(h.LikeComment))     // for owner
+	// mux.HandleFunc(urlCommentDelete, m.chain(h.DeletePost))    // for owner
+	// mux.HandleFunc(urlCommentUpdate, m.chain(h.UpdatePost))    // for owner
+	mux.HandleFunc(urlCommentLike, m.chain(h.LikeComment)) // for owner
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static", http.NotFoundHandler())
